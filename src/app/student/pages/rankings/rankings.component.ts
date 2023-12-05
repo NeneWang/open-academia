37c2c028a9d354e793387b73692842787826d40a
NelsonWang <wangnelson2@gmail.com>
2023-12-05 12:05:36 -0500

    user coursesgit poh

--- src/app/student/pages/rankings/rankings.component.ts
+++ src/app/student/pages/rankings/rankings.component.ts
@@ -1,6 +1,6 @@
 import { AcademiaserviceService } from 'src/app/academia/services/academiaservice.service';
 import { Component } from '@angular/core';
-import { UserAverage } from 'src/app/academia/models';
+import { Course, UserAverage } from 'src/app/academia/models';
 
 
 @Component({
@@ -10,6 +10,8 @@ import { UserAverage } from 'src/app/academia/models';
 })
 export class RankingsComponent {
   rankings: UserAverage[] | undefined;
+  selectedUser: UserAverage | undefined;
+  userCourses: Course[] = [];
 
   constructor(private academiaserviceService: AcademiaserviceService) { 
     this.academiaserviceService.getRanking$().subscribe(
@@ -21,6 +23,19 @@ export class RankingsComponent {
 
   onRowClick(ranking: UserAverage) {
     console.log(ranking); // Replace with your logic
+    this.selectedUser = ranking;
+    // Call to get enrolled courses
+    this.academiaserviceService.getEnrolledCourses$(ranking.id).subscribe(
+      (userCourse: any) => {
+        this.userCourses = userCourse;
+        console.log('selected userCourse', userCourse)
+      }
+    )
+  }
+
+  reset(): void {
+    this.selectedUser = undefined;
+    this.userCourses = [];
   }
 
 
