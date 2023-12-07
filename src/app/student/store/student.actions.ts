5f5a313833b1a7c99ec1892791cc82e8715e0a5e
NelsonWang <wangnelson2@gmail.com>
2023-12-07 07:51:16 -0500

    added

--- src/app/student/store/student.actions.ts
+++ src/app/student/store/student.actions.ts
@@ -3,7 +3,20 @@ import { createAction, createActionGroup, emptyProps, props } from '@ngrx/store'
 export const studentActions = createActionGroup({
     source: 'student',
     events: {
+        // Profile
         'Load Enrolled Courses': props<{userId: number}>(),
+        'Load Enrolled Courses Success': props<{data: any}>(),
+        'Load Enrolled Courses Failure': props<{error: unknown}>(),
+
+        // Personal Enrollment
+        'Enroll Course': props<{payload: any}>(),
+        'Enroll Course Success': props<{data: any}>(),
+        'Enroll Course Failure': props<{error: unknown}>(),
+
+        'Unenroll Course': props<{payload: any}>(),
+        'Unenroll Course Success': props<{data: any}>(),
+        'Unenroll Course Failure': props<{error: unknown}>(),
+
     }
 
 });
